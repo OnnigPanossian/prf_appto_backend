980d1a0be52973aaa9a35343e0434f453dcf4741
OnnigPanossian <onnig.panossian@mercadolibre.com>
2021-10-11 15:57:55 -0300

    add license model

---
+++ models/license.js
@@ -0,0 +1,22 @@
+const mongoose = require('mongoose');
+
+const beautifyUnique = require('mongoose-beautiful-unique-validation');
+
+const { Schema } = mongoose;
+
+const licenseSchema = new Schema({
+  expireDate: {
+    type: Date,
+    required: true,
+  },
+  number: {
+    type: String,
+    required: true,
+  },
+});
+
+licenseSchema.plugin(beautifyUnique);
+
+const License = mongoose.model('Vehicle', licenseSchema);
+
+module.exports = License;
