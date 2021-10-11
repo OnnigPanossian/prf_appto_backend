78c3f16aacce9f0730d2bb1922d5a9abd84fad24
Jorge Serrano <entrevistasmias@gmail.com>
2021-10-11 18:45:48 -0300

    remove users prefix

--- routes/users.js
+++ routes/users.js
@@ -5,14 +5,14 @@ const auth = require('../middlewares/auth');
 const userController = require('../controllers/users');
 
 // CREAR USUARIO
-router.post('/users', userController.createUser);
+router.post('/', userController.createUser);
 // LOGIN
-router.post('/users/login', userController.login);
+router.post('/login', userController.login);
 // LOGOUT
-router.post('/users/logout', auth, userController.logout);
+router.post('/logout', auth, userController.logout);
 // GET DEL USUARIO AUTENTICADO
-router.get('/users/me', auth, userController.getUser);
+router.get('/me', auth, userController.getUser);
 // DELETE DEL USUARIO AUTENTICADO
-router.delete('/users/me', auth, userController.deleteAuthUser);
+router.delete('/me', auth, userController.deleteAuthUser);
 
 module.exports = router;
