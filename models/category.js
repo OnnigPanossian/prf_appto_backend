9f9197b31c8e1e4f30a911c6338cc7c978e6a0d6
OnnigPanossian <onnig.panossian@mercadolibre.com>
2021-10-11 15:48:17 -0300

    add category model

---
+++ models/category.js
@@ -0,0 +1,49 @@
+const mongoose = require('mongoose');
+const validator = require('validator');
+
+const beautifyUnique = require('mongoose-beautiful-unique-validation');
+
+const { Schema } = mongoose;
+
+const categorySchema = new Schema({
+  code: {
+    type: String,
+    required: true,
+    trim: true,
+    lowercase: true,
+    validate(value) {
+      if (validator.isEmpty(value)) {
+        throw new Error('Please enter a category code');
+      }
+    },
+  },
+  costPerHour: {
+    type: Number,
+    min: 0,
+    required: true,
+    validate(value) {
+      if (validator.isEmpty(value)) {
+        throw new Error('Please enter a cost');
+      }
+    },
+  },
+  capacity: {
+    type: Number,
+    min: 2,
+    max: 7,
+    required: true,
+  },
+  trunkCapacity: {
+    type: Number,
+    min: 0,
+    default: 0,
+  },
+}, {
+  timestamps: true,
+});
+
+categorySchema.plugin(beautifyUnique);
+
+const Category = mongoose.model('Vehicle', categorySchema);
+
+module.exports = Category;
