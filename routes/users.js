876697c7a4aa7a0bad673b665d804d85f91515de
Jorge Serrano <entrevistasmias@gmail.com>
2021-10-25 21:48:37 -0300

    add garages routes

--- routes/users.js
+++ routes/users.js
@@ -1,18 +1,25 @@
+/**
+ * Dependencies
+ */
 const express = require('express');
-
-const router = express.Router();
 const auth = require('../middlewares/auth');
 const userController = require('../controllers/users');
 
-// CREAR USUARIO
+/**
+ * Express instance
+ */
+const router = express.Router();
+
+/**
+ * Routes
+ */
 router.post('/', userController.createUser);
-// LOGIN
 router.post('/login', userController.login);
-// LOGOUT
 router.post('/logout', auth, userController.logout);
-// GET DEL USUARIO AUTENTICADO
 router.get('/me', auth, userController.getUser);
-// DELETE DEL USUARIO AUTENTICADO
 router.delete('/me', auth, userController.deleteAuthUser);
 
+/**
+ * Expose routes
+ */
 module.exports = router;
