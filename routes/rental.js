066368e9e37f13f229526bfc187a6e43f4e37ce0
matibjolivera <matibjolivera@gmail.com>
2021-11-13 23:00:41 -0300

    Update rental.js

--- routes/rental.js
+++ routes/rental.js
@@ -4,8 +4,6 @@ const rentalController = require('../controllers/rental');
 
 const router = express.Router();
 
-router.use(auth);
-
 router.post('/pay/:id', rentalController.pay);
 
 module.exports = router;
