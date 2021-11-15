b0305670e307e9ed660aef41641119997ecf0ed5
Ariel Bonfil <61304733+ArielBonfil@users.noreply.github.com>
2021-11-15 00:20:20 -0300

    Fix/change update user (#38)
    * filter parkings by query
    
    * filter parkings by query

--- controllers/parking.js
+++ controllers/parking.js
@@ -1,17 +1,68 @@
+/* eslint-disable consistent-return */
 /**
  * Dependencies
  */
 const Parking = require('../models/parking');
 const Vehicle = require('../models/vehicle');
 
-const getAll = async (_, res) => Parking.find()
-  .then((garages) => {
-    if (!garages.length) {
-      return res.status(404).json({ message: 'Garages Not Found' });
+const getAll = async (req, res) => {
+  const { query } = req;
+
+  const filterCategory = {};
+  const filter = {};
+  Object.keys(query).map(async (key) => {
+    switch (key) {
+      case 'category':
+        const a = query[key].split(',');
+        const toComplete = [];
+        for (let i = 0; i < a.length; i++) {
+          toComplete.push(a[i]);
+        }
+        filterCategory['code'] = toComplete;
+        break;
+      default:
+        const b = query[key].split(', ');
+        const toCompleteFilter = [];
+        for (let i = 0; i < b.length; i++) {
+          toCompleteFilter.push(b[i]);
+        }
+        filter[key] = toCompleteFilter;
+        break;
     }
-    return res.status(200).json(garages);
-  })
-  .catch((error) => res.status(400).json({ message: error.message, error: error.errors }));
+  });
+
+  try {
+    const garages = await Parking.find().populate({
+      path: 'vehicles',
+      populate: {
+        path: 'category',
+      },
+      match: filter,
+    });
+
+    const toReturn = {};
+
+    // eslint-disable-next-line no-plusplus
+    for (let i = 0; i < garages.length; i++) {
+      let garage = garages[i];
+      if (garage.vehicles.length) {
+        // eslint-disable-next-line no-plusplus
+        if (filterCategory.hasOwnProperty('code')) {
+          for (let j = 0; j < filterCategory.code.length; j++) {
+            const e = filterCategory.code[j];
+            garage.vehicles = garage.vehicles.filter((vehicle) => vehicle.category.code === e[j]);
+          }
+        }
+      }
+
+      toReturn[i] = garage;
+    }
+
+    return res.status(200).json(toReturn);
+  } catch (error) {
+    res.status(400).json({ message: error.message, error: error.errors });
+  }
+};
 
 const createParking = async (req, res) => {
   const { body } = req;
